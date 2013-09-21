e2a2225eddb5db144d124040dc3f5e7b945c9cfb
Nate Tarrh <nate@natetarrh.com>
2013-09-21 17:27:30 -0400

    Fix stupid hash loop syntax

--- distance.js
+++ distance.js
@@ -5,15 +5,15 @@
 module.exports = {
 	match: function(userTags, blogTags) {
 		var counts = [];
-		for tag in userTags {
-			if tag in blogTags {
+		for (var tag in userTags) {
+			if (tag in blogTags) {
 				counts.push[[userTags[tag], blogTags[tag]]];
 			} else {
 				counts.push[[userTags[tag], 0]];
 			}
 		}
 		var sum = 0;
-		for pair in counts {
+		for (var pair in counts) {
 			sum += Math.pow((pair[0] - pair[1]), 2);		
 		}
 		return Math.sqrt(sum);
