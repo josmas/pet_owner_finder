5a3d607932c4e6f4743ca74054ecd0d8542eb1ce
josmas <josmasflores@gmail.com>
2013-09-21 14:43:24 -0400

    changes home page route

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var app = express();
 
+app.get('/', function(req, res){
+  res.send('Hello Index World');
+});
+
 app.get('/hello', function(req, res){
   res.send('Hello World');
 });
