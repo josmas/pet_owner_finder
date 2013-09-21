082070bae443e8a58829fea3d69a5417d745614d
Nate Tarrh <nate@natetarrh.com>
2013-09-21 19:34:02 -0400

    Fix iteration

--- distance.js
+++ distance.js
@@ -11,24 +11,24 @@ module.exports = {
 			} else {
 				counts.push([userTags[tag], 0]);
 			}
-		}
+		};
 
 		var sum = 0;
 		for (var i = counts.length - 1; i >= 0; i--) {
 		 	sum += Math.pow((counts[i][0] - counts[i][1]), 2);
-		}
+		};
 		return Math.sqrt(sum);
 	},
 	normalize: function(distances) {
 		var values = [];
-		for (var match in distances) {
-			values.push(match.distance);
-		}
+		for (var i = distances.length - 1; i >= 0; i--) {
+			values.push(distances[i].distance);
+		};
   		var min = values.slice(0).sort()[0];
   		var max = values.slice(0).sort().reverse()[0];
 		for (var i = distances.length - 1; i >= 0; i--) {
 			distances[i].distance = (distances[i].distance - min)/(max-min);
-		}
+		};
 		return distances;
 	}
 }
