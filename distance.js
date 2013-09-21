caee250ab63d6bcfd20314f8523b416695f3c0d7
Nate Tarrh <nate@natetarrh.com>
2013-09-21 19:30:35 -0400

    Convert normalize to receive a set of maps

--- distance.js
+++ distance.js
@@ -16,15 +16,19 @@ module.exports = {
 		var sum = 0;
 		for (var i = counts.length - 1; i >= 0; i--) {
 		 	sum += Math.pow((counts[i][0] - counts[i][1]), 2);
-		};
+		}
 		return Math.sqrt(sum);
 	},
 	normalize: function(distances) {
-  		var min = distances.slice(0).sort()[0];
-  		var max = distances.slice(0).sort().reverse()[0];
+		var values = [];
+		for (var match in distances) {
+			values.push(match.distance);
+		}
+  		var min = values.slice(0).sort()[0];
+  		var max = values.slice(0).sort().reverse()[0];
 		for (var i = distances.length - 1; i >= 0; i--) {
-			distances[i] = (distances[i] - min)/(max-min);
-		};
+			distances[i].distance = (distances[i].distance - min)/(max-min);
+		}
 		return distances;
 	}
 }
