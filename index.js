b02857e6a1734cd1ff282d28bcc7a23d10ee606c
josmas <josmasflores@gmail.com>
2013-09-21 14:52:05 -0400

    changes harcoded port for heroku

--- index.js
+++ index.js
@@ -9,5 +9,6 @@ app.get('/hello', function(req, res){
   res.send('Hello World');
 });
 
-app.listen(3000);
-console.log('Listening on port 3000');
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
