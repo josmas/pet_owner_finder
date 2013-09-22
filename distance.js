c848f8f1791c131ae366df85e5d08db04221cd96
Nate Tarrh <nate@natetarrh.com>
2013-09-22 01:03:13 -0400

    Convert tabs to spaces

--- distance.js
+++ distance.js
@@ -3,31 +3,31 @@
 
 // Expects a tag:count map from the user and a tag:count map from a blog
 module.exports = {
-	match: function(userTags, blogTags) {
-		var counts = [];
-		for (var tag in userTags) {
-			if (tag in blogTags) {
-				counts.push([userTags[tag], blogTags[tag]]);
-			} else { // user tag not present in matched blog
-				counts.push([userTags[tag], 0]);
-			}
-		};
-		// Calculate Euclidian distance between tag counts
-		var sum = 0;
-		for (var i = counts.length - 1; i >= 0; i--) {
-		 	sum += Math.pow((counts[i][0] - counts[i][1]), 2);
-		};
-		return Math.sqrt(sum);
-	},
-	// Order results - closest result gets highest score
-	normalize: function(distances) {
-		distances.sort(function(a,b) {
-			return a.distance - b.distance;
-		});
-		distances.sort();
-		for (var i = distances.length - 1; i >= 0; i--) {
-		 	distances[i].distance = i;
-		};
-		return distances;
-	}
+    match: function(userTags, blogTags) {
+        var counts = [];
+        for (var tag in userTags) {
+            if (tag in blogTags) {
+                counts.push([userTags[tag], blogTags[tag]]);
+            } else { // user tag not present in matched blog
+                counts.push([userTags[tag], 0]);
+            }
+        };
+        // Calculate Euclidian distance between tag counts
+        var sum = 0;
+        for (var i = counts.length - 1; i >= 0; i--) {
+            sum += Math.pow((counts[i][0] - counts[i][1]), 2);
+        };
+        return Math.sqrt(sum);
+    },
+    // Order results - closest result gets highest score
+    normalize: function(distances) {
+        distances.sort(function(a,b) {
+            return a.distance - b.distance;
+        });
+        distances.sort();
+        for (var i = distances.length - 1; i >= 0; i--) {
+            distances[i].distance = i;
+        };
+        return distances;
+    }
 }
--- tagFinder.js
+++ tagFinder.js
@@ -2,77 +2,67 @@ var async = require ('async')
 var tumblr = require('tumblr.js');
 var distance = require('./distance.js');
 var client = tumblr.createClient({
-	consumer_key: 'mQ5SRRL7mbMZwBAfNxUZX9VNIrUgzeuVoCK080860XQVMM6Vah',
-	consumer_secret: 'kRNPtQzpTr9cnmt61Dl48PMr1JQ3zEJ1DEVBAgbGq3bEfO2TgO',
-	token: 'UoxLyRg9VUJi9UJOtR6K67YFV3osYclqQyhsgVbrDiYGxLZQHX',
-	token_secret: 'yxoWX6sQVWyZiChKXrCp7nCL5bwIOPyUYqQYAfz1U1zCL0lz2h'
+    consumer_key: 'mQ5SRRL7mbMZwBAfNxUZX9VNIrUgzeuVoCK080860XQVMM6Vah',
+    consumer_secret: 'kRNPtQzpTr9cnmt61Dl48PMr1JQ3zEJ1DEVBAgbGq3bEfO2TgO',
+    token: 'UoxLyRg9VUJi9UJOtR6K67YFV3osYclqQyhsgVbrDiYGxLZQHX',
+    token_secret: 'yxoWX6sQVWyZiChKXrCp7nCL5bwIOPyUYqQYAfz1U1zCL0lz2h'
 });
 
 var getBlogs = function(pet, userBlog) {
-	client.tagged(pet, function(err, posts) {
-		blogs = [];
-		var userObj = {};
-		userObj["blogTitle"] = userBlog;
-		userObj["imgUrl"] = '';
-		userObj["tagMap"] = {};
-		userObj["distance"] = -1;
-		userObj["pet"] = pet;
-		userObj["user"] = true;
-		blogs.push(userObj);
-		posts.forEach(function(post) {
-			var blogObj = {};
-			blogObj["blogTitle"] = post.blog_name;
-			blogObj["imgUrl"] = '';
-			blogObj["tagMap"] = {};
-			blogObj["distance"] = -1;
-			blogObj["pet"] = pet;
-			blogs.push(blogObj);
-		});
-		async.map(blogs, getTags, function(err, results) {
-			var userTags = {};
-			results.forEach(function(obj) {
-				if ("user" in obj) userTags = obj["tagMap"];
-			});
-			console.log(getDistances(userTags, results));
-		});
-	});
+    client.tagged(pet, function(err, posts) {
+        blogs = [];
+        var userObj = {};
+        userObj["blogTitle"] = userBlog;
+        userObj["imgUrl"] = '';
+        userObj["tagMap"] = {};
+        userObj["distance"] = -1;
+        userObj["pet"] = pet;
+        userObj["user"] = true;
+        blogs.push(userObj);
+        posts.forEach(function(post) {
+            var blogObj = {};
+            blogObj["blogTitle"] = post.blog_name;
+            blogObj["imgUrl"] = '';
+            blogObj["tagMap"] = {};
+            blogObj["distance"] = -1;
+            blogObj["pet"] = pet;
+            blogs.push(blogObj);
+        });
+        async.map(blogs, getTags, function(err, results) {
+            var userTags = {};
+            results.forEach(function(obj) {
+                if ("user" in obj) userTags = obj["tagMap"];
+            });
+            console.log(getDistances(userTags, results));
+        });
+    });
 }
 
 var getTags = function (blogObj, doneCallback) {
-	client.posts(blogObj.blogTitle, function(err, data) {
-		data.posts.forEach(function(post) {
-			if (post.type == "photo" && post.tags.indexOf(blogObj.pet) != -1) {
-				blogObj["imgUrl"] = post.photos[0]['alt_sizes'][0]['url'];
-			}
-			post.tags.forEach(function(tag) {
-				if (Object.keys(blogObj["tagMap"]).indexOf(tag) == -1) {
-					blogObj["tagMap"][tag] = 1;
-				} else {
-					blogObj["tagMap"][tag]++;
-				}
-			});
-		});
-		return doneCallback(null, blogObj);
-	});
+    client.posts(blogObj.blogTitle, function(err, data) {
+        data.posts.forEach(function(post) {
+            if (post.type == "photo" && post.tags.indexOf(blogObj.pet) != -1) {
+                blogObj["imgUrl"] = post.photos[0]['alt_sizes'][0]['url'];
+            }
+            post.tags.forEach(function(tag) {
+                if (Object.keys(blogObj["tagMap"]).indexOf(tag) == -1) {
+                    blogObj["tagMap"][tag] = 1;
+                } else {
+                    blogObj["tagMap"][tag]++;
+                }
+            });
+        });
+        return doneCallback(null, blogObj);
+    });
 }
 
 var getDistances = function (userTags, blogs) {
-	blogs.forEach(function(obj) {
-		obj["distance"] = (distance.match(userTags, obj["tagMap"]));
-	});
-	return distance.normalize(blogs);
+    blogs.forEach(function(obj) {
+        obj["distance"] = (distance.match(userTags, obj["tagMap"]));
+    });
+    return distance.normalize(blogs);
 }
 
 exports.getBlogs = getBlogs;
 exports.getTags = getTags;
 exports.getDistances = getDistances;
-
-
-
-
-
-
-
-
-
-
