67a2681d9ddfff01f37e533254c53a139bc9fd4f
Nate Tarrh <nate@natetarrh.com>
2013-09-21 22:10:57 -0400

    Documentation

--- distance.js
+++ distance.js
@@ -8,17 +8,18 @@ module.exports = {
 		for (var tag in userTags) {
 			if (tag in blogTags) {
 				counts.push([userTags[tag], blogTags[tag]]);
-			} else {
+			} else { // user tag not present in matched blog
 				counts.push([userTags[tag], 0]);
 			}
 		};
-
+		// Calculate Euclidian distance between tag counts
 		var sum = 0;
 		for (var i = counts.length - 1; i >= 0; i--) {
 		 	sum += Math.pow((counts[i][0] - counts[i][1]), 2);
 		};
 		return Math.sqrt(sum);
 	},
+	// Order results - closest result gets highest score
 	normalize: function(distances) {
 		distances.sort(function(a,b) {
 			return a.distance - b.distance;
